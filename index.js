91658cbbcdfb249135f50f2961a1819956180684
Lidia Y De La Cruz <lidcruz@hotmail.com>
2020-04-08 16:16:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function currentLine(katzDeliLine){
   } else{
     for(let i = 0; i< katzDeliLine.length; i++){
       currentLineMessage.push((i + 1 + ".") +" "+ katzDeliLine[i])
-    } return `The line is currently: ${currentLineMessag.join(", ")}`;
+    } return `The line is currently: ${currentLineMessage.join(", ")}`;
    
 
     
